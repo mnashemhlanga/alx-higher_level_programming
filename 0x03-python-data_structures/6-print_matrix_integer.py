ebbbe82ecca6f2c28b16c2fa8cd82d0f4618c8ae
mnashemhlanga <mnashemhlanga8@gmail.com>
2022-08-30 01:30:09 +0000

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix[[]]):
-    for i in matrix:
-        for j in i:
-            print('{:d}'.format(i), end=' ')
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
         print()
