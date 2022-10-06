b67cd5dff6b4370cef7bda2061e0f94358ae948f
mnashemhlanga <mnashemhlanga8@gmail.com>
2022-10-06 02:49:43 +0000

    Add via Upload

--- 0x0C-python-almost_a_circle/18-main.py
+++ 0x0C-python-almost_a_circle/18-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-""" 100-main """
+""" 18-main """
 
 from models.rectangle import Rectangle
+
 from models.square import Square
 
 if __name__ == "__main__":
@@ -11,9 +12,9 @@ if __name__ == "__main__":
     r2 = Rectangle(2, 4)
     list_rectangles_input = [r1, r2]
 
-    Rectangle.save_to_file_csv(list_rectangles_input)
+    Rectangle.save_to_file(list_rectangles_input)
 
-    list_rectangles_output = Rectangle.load_from_file_csv()
+    list_rectangles_output = Rectangle.load_from_file()
 
     for rect in list_rectangles_input:
         print("[{}] {}".format(id(rect), rect))
@@ -30,9 +31,9 @@ if __name__ == "__main__":
     s2 = Square(7, 9, 1)
     list_squares_input = [s1, s2]
 
-    Square.save_to_file_csv(list_squares_input)
+    Square.save_to_file(list_squares_input)
 
-    list_squares_output = Square.load_from_file_csv()
+    list_squares_output = Square.load_from_file()
 
     for square in list_squares_input:
         print("[{}] {}".format(id(square), square))
